e9a7905c258076bcbbd58b89907968495434f335
Observer <80362048+68756d616e@users.noreply.github.com>
2022-12-29 23:58:46 +0000

    Add files via upload

---
+++ # Consumption tracker.py
@@ -0,0 +1,43 @@
+# Consumption tracker
+
+# FURTHER UPDATES WILL BE MADE, ID LIKE TO
+
+# You shoule be able to:
+# Set targets - Daily intake, weight loss etc
+# Maybe you can scan items or receites to recieve calories intake etc
+# input your personal details relevant to your health, physical attributes
+# create daily intake plans
+# Input daily intake, example input each item or amount of calories and it updates and tracks your progress
+
+# Welcome message
+print("Welcome to your Consumption tracker")
+
+# Consumption tracker list
+tracker = ['a','b','c']
+
+while True:
+    # The initial questions provides the options view or update
+    question = input("Would you like to 'view' or 'update' your tracker? :")
+
+    # Option view the tracker
+    if question == 'view':
+        print(tracker)
+    
+    # Update your consumption tracker
+    elif question == 'update':
+        # User choosing update, provides two additioanl options, to either add or remove
+        question2 = input("Would you like to 'add' to your tracker or 'remove' :")
+        # User choosing add will allow them to append the tracker list
+        if question2 == 'add':
+            add_question = input("What would you like to add? :")
+            add = tracker.append(add_question)
+            print(f"{add_question} added")
+        elif question2 == 'remove':
+            remove_question = input("What would you like to remove? ")
+            remove = tracker.remove(remove_question)
+            print("remove")
+        else:
+            print("Please input add in lowercase or remove")
+    
+    else:
+        print("Please input in lowercase view or update")
