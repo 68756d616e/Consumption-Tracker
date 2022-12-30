115d87dd0f9705e11233e05112b6f7831d66a293
Observer <80362048+68756d616e@users.noreply.github.com>
2022-12-30 18:53:48 +0000

    Update # Consumption tracker.py

--- # Consumption tracker.py
+++ # Consumption tracker.py
@@ -14,30 +14,45 @@ print("Welcome to your Consumption tracker")
 
 # Consumption tracker list
 tracker = ['a','b','c']
+# Targert tracker list
+targets = []
 
 while True:
     # The initial questions provides the options view or update
-    question = input("Would you like to 'view' or 'update' your tracker? :")
+    question = input("""Would you like to:
+    1 - Action Tracker
+    2 - Action Targets
+    3 - Action Intake
+    Please input tracker, target or intake in lower case.
+    """)
+    
+    if question == 'tracker':
 
     # Option view the tracker
-    if question == 'view':
-        print(tracker)
-    
-    # Update your consumption tracker
-    elif question == 'update':
-        # User choosing update, provides two additioanl options, to either add or remove
-        question2 = input("Would you like to 'add' to your tracker or 'remove' :")
-        # User choosing add will allow them to append the tracker list
-        if question2 == 'add':
-            add_question = input("What would you like to add? :")
-            add = tracker.append(add_question)
-            print(f"{add_question} added")
-        elif question2 == 'remove':
-            remove_question = input("What would you like to remove? ")
-            remove = tracker.remove(remove_question)
-            print("remove")
+        if question == 'view':
+            print(tracker)
+        
+        # Update your consumption tracker
+        elif question == 'update':
+            # User choosing update, provides two additioanl options, to either add or remove
+            question2 = input("Would you like to 'add' to your tracker or 'remove' :")
+                # User choosing add will allow them to append the tracker list
+            if question2 == 'add':
+                    add_question = input("What would you like to add? :")
+                    add = tracker.append(add_question)
+                    print(f"{add_question} added")
+            elif question2 == 'remove':
+                    remove_question = input("What would you like to remove? ")
+                    remove = tracker.remove(remove_question)
+                    print("remove")
+            else:
+                    print("Please input add in lowercase or remove")
+            
         else:
-            print("Please input add in lowercase or remove")
-    
-    else:
-        print("Please input in lowercase view or update")
+            print("Please input in lowercase view or update")
+
+    elif question == 'target':
+        print("target")
+
+    elif question == 'intake':
+        print("intake")
